fb0d619622bdc56a40e04fa68003ccbcaa3ce0f5
shofiqul <acms@acms.acms>
2023-02-07 17:47:00 +0600

    adding double jump

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float jumpPower;
-    private bool boubleJump;
+    public float jumpPower = 31;
+    public int extraJump = 1;
+
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform feet;
+
+    int jumpCount = 0;
+    bool isGrounded;
+    float jumpCoolDown;
+
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        rb.freezeRotation = true;
-    }
+    void Start() {}
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = Vector2.up * jumpPower;
+            jump();
         }
     }
+
+    void jump()
+    {
+        rb.velocity = Vector2.up * jumpPower;
+        jumpCount++;
+    }
+    
 }
